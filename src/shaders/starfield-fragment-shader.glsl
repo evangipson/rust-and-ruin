9b22b3c23dadc90c1881fbd02212e6b5ebd2310e
Evan Gipson <evangipson@gmail.com>
2025-06-05 22:16:21 -0500

    Update title screen shader to be more ***starry***

--- src/shaders/starfield-fragment-shader.glsl
+++ src/shaders/starfield-fragment-shader.glsl
@@ -4,77 +4,127 @@ precision highp float;
 varying vec4 color;
 varying vec2 uv;
 varying float iTime;
+varying vec2 iMouse;
 uniform vec2 iResolution;
 uniform float direction_modifier;
 
-#define NUM_LAYERS 4.
+#define PI 3.14159
+#define NUM_LAYERS 9.
 
-mat2 Rot(float a) {
-    float s = sin(a), c = cos(a);
-    return mat2(c, -s, s, c);
+// rotate will take an angle and rotate it using:
+// [cos -sin]
+// [sin  cos]
+mat2 rotate(float angle)
+{
+    float sine = sin(angle);
+    float cosine = cos(angle);
+    return mat2(cosine, -sine, sine, cosine);
 }
 
-float Star(vec2 uv, float flare) {
-    float d = length(uv);
-    float m = .05 / d;
-
-    float rays = max(0., 1. - abs(uv.x * uv.y * 1000.));
-    m += rays * flare;
-    uv *= Rot(3.1415 / 4.);
-    rays = max(0., 1. - abs(uv.x * uv.y * 1000.));
-    m += rays * .3 * flare;
-
-    m *= smoothstep(1., .2, d);
-
-    return m;
+// hash will take in 2 numbers as a vec2 and return
+// a pseudo-random number with some funky math.
+float hash(vec2 numbers)
+{
+    numbers = fract(numbers * vec2(123.34, 456.21));
+    numbers += dot(numbers, numbers + 45.32);
+    return fract(numbers.x * numbers.y);
 }
 
-float Hash21(vec2 p) {
-    p = fract(p * vec2(123.34, 456.21));
-    p += dot(p, p + 45.32);
-    return fract(p.x * p.y);
+// getStar will draw a star given the provided radius
+// and luminosity.
+float getStar(vec2 position, float radius, float luminosity)
+{
+    // the star will emit a glow from it's position to it's radius
+    float star = radius / length(position);
+    
+    // the rays extend past the star's position, but must not surpass
+    // the local bounds of the star
+    float rays = max(0., 1. - abs(position.x * position.y * 1500.));
+    star += rays * luminosity;
+    
+    // draw a second set of rays rotated at a 45 degree angle
+    position *= rotate(PI/4.);
+    rays = max(0., 1. - abs(position.x * position.y * 2000.));
+    star += rays * luminosity * .3;
+    
+    // fade out the glow of the star so it doesn't exceed it's bounds
+    star *= smoothstep(1., .2, length(position));
+    
+    return star;
 }
 
-vec3 StarLayer(vec2 uv) {
-    vec3 col = vec3(0);
-
-    vec2 gv = fract(uv) - .5;
-    vec2 id = floor(uv);
-
-    float t = iTime * 0.1;
-    for (int y = -1; y <= 1; y++) {
-        for (int x = -1; x <= 1; x++) {
-            vec2 offs = vec2(x, y);
-
-            float n = Hash21(id + offs); // random between 0 and 1
-            float size = fract(n * 345.32);
-            float star = Star(gv - offs - vec2(n, fract(n * 42.)) + .5, smoothstep(.9, 1., size) * .6);
-            vec3 color = sin(vec3(.8, .8, .8) * fract(n * 2345.2) * 123.2) * .5 + .5;
-            color = color * vec3(0.25, 0.25, 0.20);
-            star *= sin(iTime * 3. + n * 6.2831) * .5 + 1.;
-            col += star * size * color;
+// getStarLayer will draw a bunch of stars as a layer.
+vec3 getStarLayer(vec2 center)
+{
+    // start with a black canvas
+    vec3 color = vec3(0);
+    
+    // break the layer up into the fractional component of the center
+    vec2 grid = fract(center) - 0.5;
+    
+    // take the rounded-down int of the center as a unique id for each star
+    vec2 id = floor(center);
+    
+    // loop between all neighboring stars over the grid
+    for(int y = -1; y <= 1; y++)
+    {
+        for (int x = -1; x <= 1; x++)
+        {
+            // make this star aware of where it is among the neighbors
+            vec2 offset = vec2(x, y);
+            
+            // get randomized values for each star
+            float randomNumber = hash(id + offset);
+            float randomSize = fract(randomNumber * 223.6);
+            float randomLuminosity = smoothstep(.97, 1., randomSize) * .4;
+            vec3 randomColor = sin(vec3(.2, .3, .9) * fract(randomNumber * 1122.4) * PI) * .5 + .5;
+            
+            // get the star
+            float star = getStar(
+                grid - offset - vec2(randomNumber, fract(randomNumber * 34144.)) + .5,
+                0.04 * randomSize,
+                randomLuminosity
+            );
+            
+            // twinkle, twinkle, little star
+            star *= sin(iTime * 3. + randomNumber * PI * 2.) * .1 + .7;
+            
+            // add the star to the total color
+            color += star * (randomColor * vec3(0.95, 0.8, 0.6));
         }
     }
-    return col;
+    
+    return color;
 }
 
+// main is the entry point of the shader.
 void main()
 {
-    vec2 uv = (gl_FragCoord.xy - .5 * iResolution.xy) / iResolution.y;
-    float t = iTime * .02;
-
-    float speed = 3.0;
-    vec2 direction = vec2(-0.25 + direction_modifier, -1.0) * speed;
-
-    uv += direction;
-    vec3 col = vec3(0);
-
-    for (float i = 0.; i < 1.; i += 1. / NUM_LAYERS) {
-        float depth = fract(i+t);
+    // get a relative speed of movement based on the shader timer
+    float time = iTime * .03;
+    
+    // get the relative position of the mouse
+    vec2 mouse = (iMouse.xy - iResolution.xy * .5) / iResolution.y;
+    
+    // get the center of the total area
+    vec2 center = (gl_FragCoord.xy - 0.5 * iResolution.xy) / iResolution.y;
+    
+    // rotate the canvas based on relative mouse position and speed
+    center += mouse * 2.;
+    center *= rotate(time * 0.3);
+    
+    // start with a black canvas
+    vec3 color = vec3(0);
+    
+    // draw star layers
+    for (float i = 0.; i < 1.; i += 1./NUM_LAYERS)
+    {
+        float depth = fract(i + time);
         float scale = mix(20., .5, depth);
-        float fade = depth * smoothstep(1., .9, depth);
-        col += StarLayer(uv * scale + i * 453.2) * fade;
+        float fade = depth * smoothstep(1., .97, depth);
+        color += getStarLayer(center * scale + i * 435.2) * fade;
     }
-
-    gl_FragColor = vec4(col, 1.0);
+    
+    // set the output to the star layer
+    gl_FragColor = vec4(color, 1.0);
 }
--- src/shaders/starfield-vertex-shader.glsl
+++ src/shaders/starfield-vertex-shader.glsl
@@ -3,12 +3,15 @@ attribute vec3 position;
 attribute vec2 texcoord;
 attribute vec4 color0;
 varying float iTime;
+varying vec2 iMouse;
 
 uniform mat4 Model;
 uniform mat4 Projection;
 uniform vec4 _Time;
+uniform vec2 _Mouse;
 
 void main() {
     gl_Position = Projection * Model * vec4(position, 1);
     iTime = _Time.x;
+    iMouse = _Mouse.xy;
 }
